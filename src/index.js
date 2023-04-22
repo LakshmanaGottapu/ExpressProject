99a9fb55cd511511200220acd9f290779bb70bb5
LakshmanGottapu <sailakshman120@gmail.com>
2023-04-22 11:21:07 +0530

    added courses hbs file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const requests = require('requests');
 const homePath = path.join(__dirname,"../public/home.html");
-const cssPath = path.join(__dirname,"../public/external.css");
+const cssPath = path.join(__dirname,"../public/css/style.css");
 const staticPath = path.join(__dirname,"../public");
 // const htmlFileContent = fs.readFileSync(homePath,"utf-8");
 // const cssFileContent = fs.readFileSync(cssPath,"utf-8");
@@ -23,27 +23,14 @@ app.get("/",(req,res)=>{
     // console.log("sending the response.......");
     res.render("index");
 });
-
-app.get("/about",(req,res)=>{
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=05e9d0f2bbf1bb8211140aa1d0e35935`;
-    requests(url)
-    .on('data',(chunk)=>{
-        const responseObject = JSON.parse(chunk);
-        console.log(`City is ${responseObject.name} temperature is ${responseObject.main.temp}`);
-        res.write(responseObject.name);
-    })
-    .on('end',(err)=>{
-        if(err) console.log(err);
-        res.send();
-    })
-});
-app.get("/about/*",(req,res)=>{
-    res.render("404",{
-        errorComment: "Oops the page you requested is not found in about page"
-    });
-});
 app.get("/home",(req,res)=>{
     res.render("home");
+});
+app.get("/courses",(req,res)=>{
+    res.render("courses");
+});
+app.get("/about",(req,res)=>{
+    res.render("about");
 });
 app.get("/login",(req,res)=>{
     res.render("login");
@@ -51,11 +38,15 @@ app.get("/login",(req,res)=>{
 app.get("/milestones",(req,res)=>{
     res.render("successpage");
 });
-app.get('/css/style.css', (req, res)=> {
+app.get('/style.css', (req, res)=> {
     res.type('text/css');
-    res.sendFile(__dirname+'../public/css/style.css');
+    res.sendFile(cssPath);
 });
-
+app.get("/*",(req,res)=>{
+    res.render("404",{
+        errorComment: "Oops the page you requested is not found in about page"
+    });
+});
 // server.get("/", (req,res)=>{
 //     const content= `
 //     <html>
